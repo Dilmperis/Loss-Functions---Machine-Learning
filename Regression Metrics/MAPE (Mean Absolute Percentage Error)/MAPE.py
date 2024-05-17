feaa0f51c45bd50f944f20361324628a4b75986e
Giannis Dilmperis <dil.john2@hotmail.com>
2024-05-18 00:15:33 +0300

    Update MAPE.py

--- Regression Metrics/MAPE (Mean Absolute Percentage Error)/MAPE.py
+++ Regression Metrics/MAPE (Mean Absolute Percentage Error)/MAPE.py
@@ -1,14 +1,21 @@
+import torch
 from tensorflow.keras import losses
+import numpy as np
 np.random.seed(0)
 
 def custom_mape(y_pred, y_true):  # Custom
     return np.mean((np.abs(y_pred - y_true) / np.abs(y_pred)) * 100)
 
+# Given a toy target and prediction of 5 samples:
+y_pred = np.array([.2, .4, .7, 1., 1.3])
+y_true = np.array([.5, .4, .6, 1.5, 2])
 
-mape_custom = custom_mape(y_pred, y_true)
+tensor_pred = torch.tensor(y_pred)
+tensor_true = torch.tensor(y_true)
 
+# Losses
+mape_custom = custom_mape(y_pred, y_true) # Custom
 # Pytorch DOES NOT HAVE Built-in MAPE
-
 mape_tf = losses.MAPE(y_pred, y_true)  # Tensorflow
 
 # Print Statements
