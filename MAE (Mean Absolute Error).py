3d39dc28257b18b3b5547133e4fb6dddcfbfde3c
Giannis Dilmperis <dil.john2@hotmail.com>
2024-05-17 23:37:16 +0300

    Add files via upload

---
+++ MAE (Mean Absolute Error).py
@@ -0,0 +1,15 @@
+def custom_mae(y_pred, y_true):                 # Custom
+    return np.mean(np.abs(y_pred - y_true))
+
+loss_custom = custom_mae(y_pred, y_true)
+
+l1_loss = L1Loss()                               # Pytorch
+loss_torch = l1_loss(tensor_pred, tensor_true)
+
+tf_loss = losses.MAE(y_pred, y_true)             # Tensorflow
+
+
+#Print Statements
+print(f'Custom MAE loss:\t {loss_custom}')
+print(f'Torch MAE loss:\t\t {loss_torch}')
+print(f'TensorFlow MAE loss:\t {tf_loss}')
