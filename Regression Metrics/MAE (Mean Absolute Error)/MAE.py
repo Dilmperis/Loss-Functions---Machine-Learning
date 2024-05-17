863b4395ab0dbe0b15840322dee0e09956a98d8c
Giannis Dilmperis <dil.john2@hotmail.com>
2024-05-18 00:16:34 +0300

    Update MAE.py

--- Regression Metrics/MAE (Mean Absolute Error)/MAE.py
+++ Regression Metrics/MAE (Mean Absolute Error)/MAE.py
@@ -1,15 +1,23 @@
+import torch
 from torch.nn import L1Loss
 from tensorflow.keras import losses
+import numpy
 np.random.seed(0)
 
 def custom_mae(y_pred, y_true):                 # Custom
     return np.mean(np.abs(y_pred - y_true))
 
-loss_custom = custom_mae(y_pred, y_true)
+# Given a toy target and prediction of 5 samples:
+y_pred = np.array([.2, .4, .7, 1., 1.3])
+y_true = np.array([.5, .4, .6, 1.5, 2])
 
+tensor_pred = torch.tensor(y_pred)
+tensor_true = torch.tensor(y_true)
+
+# Losses
+loss_custom = custom_mae(y_pred, y_true)         # Custom
 l1_loss = L1Loss()                               # Pytorch
 loss_torch = l1_loss(tensor_pred, tensor_true)
-
 tf_loss = losses.MAE(y_pred, y_true)             # Tensorflow
 
 
