f722e63633588d7ee8b35e42ab3e4233050a186b
Giannis Dilmperis <dil.john2@hotmail.com>
2024-05-18 00:12:56 +0300

    Update MSE.py

--- Regression Metrics/MSE - Mean Squared Error/MSE.py
+++ Regression Metrics/MSE - Mean Squared Error/MSE.py
@@ -1,16 +1,23 @@
+import torch
 from torch.nn import MSELoss
 from tensorflow.keras import losses
+import numpy as np
 np.random.seed(0)
 
 def custom_mse(y_pred, y_true):  # Custom
     return np.mean((y_pred - y_true) ** 2)
 
+# Given a toy target and prediction of 5 samples:
+y_pred = np.array([.2, .4, .7, 1., 1.3])
+y_true = np.array([.5, .4, .6, 1.5, 2])
 
-mse_custom = custom_mse(y_pred, y_true)
+tensor_pred = torch.tensor(y_pred)
+tensor_true = torch.tensor(y_true)
 
+#Losses
+mse_custom = custom_mse(y_pred, y_true) # Custom
 MSE_torch = MSELoss()  # Pytorch
 mse_torch = MSE_torch(tensor_pred, tensor_true)
-
 mse_tf = losses.MSE(y_pred, y_true)  # Tensorflow
 
 # Print Statements
